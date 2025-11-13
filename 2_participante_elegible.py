281d4230eb7cd89dd1fb6564cb805d987daabf7e
pipetogra95 <pipetoro09@gmail.com>
2025-11-13 11:11:05 -0500

    Refactor eligibility checks for participants

--- 2_participante_elegible.py
+++ 2_participante_elegible.py
@@ -1,13 +1,28 @@
-while True:
-    try:
-        edad=int(input("Ingrese su edad: "))
-        licencia=(input("¿Tiene licencia de conducción vigente?si/no"))
-        vehiculo=(input("¿Tiene vehículo propio?si/no"))
-        if edad<0:
-            print("Valor inválido")
-        elif edad<18:
-            print("No puede participar")
-            break
-    except ValueError:
-        print("Debe ser un número entero mayor a cero ")
-        
+edad=input("Ingrese su edad: ")
+while not edad.isdigit():
+    print("Debe ingresar un valor entero igual o mayor a cero")
+    edad=input("Ingrese su edad:")
+edad=int(edad)
+if edad<18:
+    print("No apto")
+elif edad>=18:
+    licencia=input("¿Tiene licencia de conducción vigente?si/no: ").lower()
+    while licencia!="si" and licencia!="no":
+        print("Solo conteste si o no")
+        licencia=input("¿Tiene licencia de conducción vigente?si/no: ")
+    if licencia=="no":
+        print("No apto")
+    else:
+        vehiculo=input("¿Tiene vehiculo propio?si/no: ").lower()
+        while vehiculo!="si" and vehiculo!="no":
+            print("Solo conteste si o no")
+            vehiculo=input("¿Tiene vehiculo propio?si/no: ")
+        if vehiculo=="no":
+            prestamo=input("¿Tiene manera de competir con un auto prestado?si/no: ")
+            while prestamo!="si" and prestamo!="no":
+                print("Solo conteste si o no")
+                prestamo=input("¿Tiene manera de competir con un auto prestado?si/no: ")
+            if prestamo=="si":
+                print("Apto")
+            else:
+                print("No apto")
